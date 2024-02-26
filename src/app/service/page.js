9405c46a194735b3a224deaac32b348363d87d4c
Md Alamin <alaminduc516@gmail.com>
2024-02-26 23:45:49 +0600

    Added services in the services page

--- src/app/service/page.js
+++ src/app/service/page.js
@@ -1,5 +1,5 @@
 import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
-import React from "react";
+import Image from "next/image";
 
 const Service = () => {
   return (
@@ -14,6 +14,259 @@ const Service = () => {
             <h2 className="text-[30px] font-semibold leading-[45px]">
               We Provide BestWeb design services
             </h2>
+          </div>
+          <div className="flex flex-col gap-[50px]">
+            {/* Row 1 */}
+            <div className="flex gap-[30px]">
+              {/* service-card  Start*/}
+              <div className="service-card">
+                <h2 className="text-[26px] font-semibold leading-[39px] mb-5">
+                  BUILD & LAUNCH WITHOUT PROBLEMS
+                </h2>
+                <p className="text-base text-[#9D9D9D]">
+                  Increase Your Team's Productivity And Save Time With A Bot
+                  That Answers And Fields Customer Inquiries
+                </p>
+                <div className="mt-[54px] flex flex-col gap-[20px]">
+                  <div className="flex gap-[30px]">
+                    <div className="service-image-container">
+                      <Image
+                        width={358}
+                        height={164}
+                        src="/images/services/1.png"
+                        alt="Service 1 Description"
+                        className="h-[164px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                    <div className="service-image-container">
+                      <Image
+                        width={143}
+                        height={164}
+                        src="/images/services/2.png"
+                        alt="Service 2 Description"
+                        className="h-[164px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex gap-[30px]">
+                    <div>
+                      <Image
+                        height={263}
+                        width={257}
+                        src={"/images/services/3.png"}
+                        alt=""
+                        className="h-[263px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                    <div>
+                      <Image
+                        height={263}
+                        width={248}
+                        src={"/images/services/4.png"}
+                        alt=""
+                        className="h-[263px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* service-card  End*/}
+
+              {/* service-card  Start*/}
+              <div className="service-card">
+                <h2 className="text-[26px] font-semibold leading-[39px] mb-5">
+                  BUILD & LAUNCH WITHOUT PROBLEMS
+                </h2>
+                <p className="text-base text-[#9D9D9D]">
+                  Increase Your Team's Productivity And Save Time With A Bot
+                  That Answers And Fields Customer Inquiries
+                </p>
+                <div className="mt-[54px] flex flex-col gap-[20px]">
+                  <div className="flex gap-[30px]">
+                    <div className="service-image-container">
+                      <Image
+                        width={358}
+                        height={164}
+                        src="/images/services/5.png"
+                        alt="Service 1 Description"
+                        className="h-[164px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                    <div className="service-image-container">
+                      <Image
+                        width={143}
+                        height={164}
+                        src="/images/services/6.png"
+                        alt="Service 2 Description"
+                        className="h-[164px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex gap-[30px]">
+                    <div>
+                      <Image
+                        height={263}
+                        width={257}
+                        src={"/images/services/7.png"}
+                        alt=""
+                        className="h-[263px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                    <div>
+                      <Image
+                        height={263}
+                        width={248}
+                        src={"/images/services/8.png"}
+                        alt=""
+                        className="h-[263px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* service-card  End*/}
+            </div>
+
+            {/* Row 2 */}
+            <div className="flex gap-[30px]">
+              {/* service-card  Start*/}
+              <div className="service-card">
+                <h2 className="text-[26px] font-semibold leading-[39px] mb-5">
+                  BUILD & LAUNCH WITHOUT PROBLEMS
+                </h2>
+                <p className="text-base text-[#9D9D9D]">
+                  Increase Your Team's Productivity And Save Time With A Bot
+                  That Answers And Fields Customer Inquiries
+                </p>
+                <div className="mt-[54px] flex flex-col gap-[20px]">
+                  <div className="flex gap-[30px]">
+                    <div className="service-image-container">
+                      <Image
+                        width={358}
+                        height={164}
+                        src="/images/services/9.png"
+                        alt="Service 1 Description"
+                        className="h-[164px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                    <div className="service-image-container">
+                      <Image
+                        width={143}
+                        height={164}
+                        src="/images/services/10.png"
+                        alt="Service 2 Description"
+                        className="h-[164px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex gap-[30px]">
+                    <div>
+                      <Image
+                        height={263}
+                        width={257}
+                        src={"/images/services/11.png"}
+                        alt=""
+                        className="h-[263px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                    <div>
+                      <Image
+                        height={263}
+                        width={248}
+                        src={"/images/services/12.png"}
+                        alt=""
+                        className="h-[263px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* service-card  End*/}
+
+              {/* service-card  Start*/}
+              <div className="service-card">
+                <h2 className="text-[26px] font-semibold leading-[39px] mb-5">
+                  BUILD & LAUNCH WITHOUT PROBLEMS
+                </h2>
+                <p className="text-base text-[#9D9D9D]">
+                  Increase Your Team's Productivity And Save Time With A Bot
+                  That Answers And Fields Customer Inquiries
+                </p>
+                <div className="mt-[54px] flex flex-col gap-[20px]">
+                  <div className="flex gap-[30px]">
+                    <div className="service-image-container">
+                      <Image
+                        width={358}
+                        height={164}
+                        src="/images/services/13.png"
+                        alt="Service 1 Description"
+                        className="h-[164px]"
+                      />
+                    </div>
+                    <div className="service-image-container">
+                      <Image
+                        width={143}
+                        height={164}
+                        src="/images/services/14.png"
+                        alt="Service 2 Description"
+                        className="h-[164px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex gap-[30px]">
+                    <div>
+                      <Image
+                        height={263}
+                        width={257}
+                        src={"/images/services/15.png"}
+                        alt=""
+                        className="h-[263px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                    <div>
+                      <Image
+                        height={263}
+                        width={248}
+                        src={"/images/services/16.png"}
+                        alt=""
+                        className="h-[263px]"
+                        blurDataURL="/blur.png"
+                        placeholder="blur"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* service-card  End*/}
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/Services/ServiceCard.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+
+const ServiceCard = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default ServiceCard;
