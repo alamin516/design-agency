0975aee5f10ad41fff5f851e4dfd52976aa0ee68
Md Alamin <alaminduc516@gmail.com>
2024-03-06 18:32:19 +0600

    Metadata update and added Baseurl

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,31 +3,39 @@ import "./globals.css";
 import Newsletter from "@/components/Newsletter/Newsletter";
 import Footer from "@/components/Footer/Footer";
 import NextTopLoader from "nextjs-toploader";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export async function generateMetadata(){
-  return{
-      title: "Design Agency",
-      description: "Design Agency - Let's build a awesome work together",
-      keywords: "design agency, web agency",
-      openGraph: {
-          images: "/Design Agency Let's build a awesome work together.png",
+export async function generateMetadata() {
+  return {
+    title: "Design Agency",
+    description: "Design Agency - Let's build a awesome work together",
+    keywords: "design agency, web agency",
+    metadataBase: new URL("https://acme.com"),
+    alternates: {
+      canonical: "/",
+      languages: {
+        "en-US": "/en-US",
       },
-  }
+    },
+    openGraph: {
+      images: "/Design Agency Let's build a awesome work together.png",
+    },
+  };
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta name={process.env.CONTENT_NAME} content={process.env.CONTENT_KEY} />
+        <meta
+          name={process.env.CONTENT_NAME}
+          content={process.env.CONTENT_KEY}
+        />
       </Head>
       <body className={inter.className}>
-        <NextTopLoader
-        color="#20B15A"
-        showSpinner={false}
-        />
+        <NextTopLoader color="#20B15A" showSpinner={false} />
         {children}
         <Newsletter />
         <Footer />
