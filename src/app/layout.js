5b454c98fd1b5a941a084f93fbce23bd50a80f6a
Md Alamin <alaminduc516@gmail.com>
2024-03-06 17:54:19 +0600

    Delete default metadata from root layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,6 @@ import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Design Agency",
-  description: "Let's build a awesome work together",
-};
-
 export async function generateMetadata(){
   return{
       title: "Design Agency",
