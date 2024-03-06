d2270cd4350169814d53fc09c6e3780c0ddfaad1
Md Alamin <alaminduc516@gmail.com>
2024-03-06 17:51:46 +0600

    Root metadata added

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,17 @@ export const metadata = {
   description: "Let's build a awesome work together",
 };
 
+export async function generateMetadata(){
+  return{
+      title: "Design Agency",
+      description: "Design Agency - Let's build a awesome work together",
+      keywords: "design agency, web agency",
+      openGraph: {
+          images: "/Design Agency Let's build a awesome work together.png",
+      },
+  }
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
